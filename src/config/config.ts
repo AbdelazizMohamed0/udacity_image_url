ba63a6b278f28d746f2dcce3d8ccd0c8816aeaf2
abdelazizmohamed0 <a.mohamed@qoyod.com>
2022-11-26 23:53:33 +0200

    add project files

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udacitydb2",
+    "password": "123456789",
+    "database": "",
+    "host": "udacitydb2",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "secondproject-dev-ud"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
